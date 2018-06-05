b0ee17a034de19190e745e34a113f7775ed1411b
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2018-06-05 23:31:40 +0100

    Initial front end folder structure + initial setup of login functionality

---
+++ client/src/app/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import LoginPage from './pages/LoginPage/LoginPage';
+
+const app = () => {
+	return <LoginPage />;
+};
+
+export default app;
---
+++ client/src/app/components/Login/Login.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './styles.css';
+import { withFormik, Form, Field } from 'formik';
+import Yup from 'yup';
+
+// these are props passed from withFormik
+const Login = ({ values, errors, touched }) => (
+	<Form>
+		<div>
+			{touched.email && errors.email && <p>{errors.email}</p>}
+			<Field type="email" name="email" placeholder="Email" />
+		</div>
+		<div>
+			{touched.password && errors.password && <p>{errors.password}</p>}
+			<Field type="password" name="password" placeholder="Password" />
+		</div>
+		<button>Go</button>
+	</Form>
+);
+
+// withFormik HOC
+const FormikLogin = withFormik({
+	mapPropsToValues({ email, password }) {
+		return {
+			email: email || '',
+			password: password || '',
+		};
+	},
+	validationSchema: Yup.object().shape({
+		email: Yup.string()
+			.email('Email not valid')
+			.required('Email is required'),
+		password: Yup.string()
+			.min(9, 'Password must be 9 characters or longer')
+			.required('Password is required'),
+	}),
+	handleSubmit(values, { resetForm, setSubmitting }) {
+		console.log(values);
+		alert('Values outputed to Console');
+		resetForm();
+		setSubmitting(false);
+	},
+})(Login);
+
+export default FormikLogin;
