b62313ee128a828a91b87fe1be33f04d427a2e1a
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2018-06-11 23:12:10 +0100

    added validation for email and password entries

--- client/src/app/components/Register/Register.js
+++ client/src/app/components/Register/Register.js
@@ -8,13 +8,16 @@ import axios from 'axios';
 const Register = ({ values, errors, touched }) => (
 	<Form>
 		<div>{touched.email && errors.email && <p>{errors.email}</p>}</div>
-		<Field type="email" name="email" placeholder="Email" />
+		<Field type="email" name="email" placeholder="Email Address" />
+
+		<div>{touched.emailConfirm && errors.emailConfirm && <p>{errors.emailConfirm}</p>}</div>
+		<Field type="email" name="emailConfirm" placeholder="Confirm email" />
 
 		<div>{touched.password && errors.password && <p>{errors.password}</p>}</div>
 		<Field type="password" name="password" placeholder="Password" />
 
-		<div>{touched.password2 && errors.password2 && <p>{errors.password2}</p>}</div>
-		<Field type="password" name="password2" placeholder="Confirm password" />
+		<div>{touched.passwordConfirm && errors.passwordConfirm && <p>{errors.passwordConfirm}</p>}</div>
+		<Field type="password" name="passwordConfirm" placeholder="Confirm password" />
 
 		<div>{touched.fullName && errors.fullName && <p>{errors.fullName}</p>}</div>
 		<Field type="text" name="fullName" placeholder="Full name" />
@@ -28,11 +31,12 @@ const Register = ({ values, errors, touched }) => (
 
 // withFormik HOC
 const FormikRegister = withFormik({
-	mapPropsToValues({ email, password, password2, fullName, username }) {
+	mapPropsToValues({ email, emailConfirm, password, passwordConfirm, fullName, username }) {
 		return {
 			email: email || '',
+			emailConfirm: emailConfirm || '',
 			password: password || '',
-			password2: password2 || '',
+			passwordConfirm: passwordConfirm || '',
 			fullName: fullName || '',
 			username: username || '',
 		};
@@ -41,21 +45,25 @@ const FormikRegister = withFormik({
 		email: Yup.string()
 			.email('Email not valid')
 			.required('Email is required'),
+		emailConfirm: Yup.mixed().test('Match', 'Email addresses do not match', function() {
+			return this.parent.emailConfirm === this.parent.email;
+		}),
 		password: Yup.string()
 			.min(8, 'Password must be 8 characters or longer')
 			.required('Password is required'),
-		password2: Yup.string()
-			.min(8, 'Password must be 8 characters or longer')
-			.required('Password is required'),
+		passwordConfirm: Yup.mixed().test('Match', 'Passwords do not match', function() {
+			return this.parent.passwordConfirm === this.parent.password;
+		}),
 		fullName: Yup.string().required('Your name is required'),
 		username: Yup.string().required('Your username is required'),
 	}),
 
 	handleSubmit(values, { resetForm, setSubmitting }) {
-		const { email, password, password2, fullName, username } = values;
+		const { email, emailConfirm, password, passwordConfirm, fullName, username } = values;
 
+		console.log(password);
 		axios
-			.post('/api/users/register', { email, password, password2, fullName, username })
+			.post('/api/users/register', { email, emailConfirm, password, passwordConfirm, fullName, username })
 			.then(result => {
 				console.log(result);
 			})
