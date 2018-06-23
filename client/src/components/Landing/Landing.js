dcca8f60c7c5e33727979dd09c9f97da4e010de1
Darko Tasevski <darktasevski@gmail.com>
2018-06-23 15:57:10 +0200

    Initial landing page

---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Landing = () => {
+	return (
+		<div className="landing">
+			<div className="container">
+				<h1 className="heading heading--primary landing__title">Your Morning Routine</h1>
+				<h2 className="heading heading--tertiary landing__subtitle">
+					Some people dream of success, while other people get up every morning and make it happen.
+				</h2>
+			</div>
+		</div>
+	);
+};
+
+export default Landing;
